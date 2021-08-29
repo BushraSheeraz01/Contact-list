7d8b239d9cd5f2cb2193bc9c70f8c9047546204f
Bushra Sheeraz <bushrasheeraz122@gmail.com>
2021-08-30 01:23:23 +0500

    completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PersonInfo from './components/PersonInfo';
+
+function App() {
+  return (
+    <div className="App">
+        <PersonInfo />
+      </div>
+  );
+
+}
+
+export default App;
